02864f61454a8680923fb19d2ebc245908bf5cf0
Marcelo Norita <marnori2001@gmail.com>
2024-07-16 20:38:04 -0300

    Feat: add Languages and validations

--- src/CashFlow.Application/UseCases/Users/Register/RegisterUserValidator.cs
+++ src/CashFlow.Application/UseCases/Users/Register/RegisterUserValidator.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Communication.Requests;
+using CashFlow.Exception;
 using FluentValidation;
 
 namespace CashFlow.Application.UseCases.Users.Register;
@@ -6,9 +7,9 @@ internal class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson
 {
     public RegisterUserValidator()
     {
-        RuleFor(user => user.UserName).NotEmpty().WithMessage("The UserName is required");
-        RuleFor(user => user.Email).NotEmpty().WithMessage("The Email is required");
-        RuleFor(user => user.Password).NotEmpty().WithMessage("The Password is required");
-        RuleFor(user => user.Type).IsInEnum().WithMessage("The Type is not valid");
+        RuleFor(user => user.UserName).NotEmpty().WithMessage(ResourceErrorMessages.USERNAME_REQUIRED);
+        RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceErrorMessages.EMAIL_REQUIRED);
+        RuleFor(user => user.Password).NotEmpty().WithMessage(ResourceErrorMessages.PASSWORD_REQUIRED);
+        RuleFor(user => user.Type).IsInEnum().WithMessage(ResourceErrorMessages.TYPE_INVALID);
     }
 }
